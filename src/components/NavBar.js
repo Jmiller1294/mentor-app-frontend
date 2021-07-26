a734a0828246e047f3a0f27da64c26d5446334b0
Justin Miller <jayman1294@gmail.com>
2021-07-25 20:32:34 -0400

    added login button and styling for login button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Nav, NavLink } from './styled/NavBarElements';
+import { Nav, NavLink, NavBtn, NavBtnLink } from './styled/NavBarElements';
 
  
 const NavBar = () => {
   return(
     <>
       <Nav>
-        <NavLink to="/mentors">Login</NavLink>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/mentors">Mentors</NavLink>
         <NavLink to="/events">Events</NavLink>
         <NavLink to="/about">About</NavLink>
         <NavLink to="/registration">Signup</NavLink>
-        <NavLink to="/login">Login</NavLink>
         <NavLink to="/accounts">Account</NavLink>
+        <NavBtn>
+          <NavBtnLink to="/login">Login</NavBtnLink>
+        </NavBtn>
       </Nav>
     </>
   )
--- src/components/styled/NavBarElements.js
+++ src/components/styled/NavBarElements.js
@@ -3,13 +3,37 @@ import { NavLink as Link } from "react-router-dom";
 
 export const Nav = styled.nav`
   background: blue;
-  height: 85px;
+  height: 70px;
+  padding: 25px;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
 `
 
 export const NavLink = styled(Link)`
+  margin: 0 50px 0 50px;
   color: black;
   display: flex;
   text-decoration: none;
+`
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+`
+export const NavBtnLink = styled(Link)`
+  border-radius: 10px;
+  background: #808080;
+  padding: 5px;
+  height: 30px;
+  text-align: center;
+  width: 100px;
+  color: #000000;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  margin-left: 50px;
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #808080;
+  }
 `
