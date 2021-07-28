aa71bfd25e6810779f9d056c51a6888a3833f677
Justin Miller <jayman1294@gmail.com>
2021-07-27 23:02:19 -0400

    added faker to get avatar images, added styling for mentors

--- src/containers/MentorsPage.js
+++ src/containers/MentorsPage.js
@@ -2,12 +2,23 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Grid } from '../components/styled/Grid';
 
+let faker = require('faker')
+
 const List = styled.ul`
   list-style: none;
+  padding: 0;
 `
 
 const ListItem = styled.li`
+  display: flex;
+  height: 200px;
+  border: 1px solid #f8f7f2;
+`
 
+const AvatarImage = styled.img`
+  height: 100px;
+  width: 5%;
+  border-radius: 50px;
 `
 
 const MentorsPage = () => {
@@ -20,10 +31,17 @@ const MentorsPage = () => {
     .catch((error) => console.error('Error:', error))
   },[])
 
+
   return(
     <Grid>
       <List>
-        {mentors.map(mentor => <ListItem>{mentor.name}</ListItem>)}
+        {mentors.slice(1, 10).map(mentor => 
+        <ListItem key={mentor.id}>
+          <AvatarImage alt="avatar" src={faker.image.avatar()}></AvatarImage>
+          {mentor.name} <br></br>
+          {mentor.title} <br></br>
+          {mentor.field}
+        </ListItem>)}
       </List>
     </Grid>
   )
