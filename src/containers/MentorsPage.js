1ac5154385476529e224892e072cf34c600ebce6
Justin Miller <jayman1294@gmail.com>
2021-07-27 21:48:18 -0400

    made fetch request to backend and added list of mentors to mentorsPage

--- src/containers/MentorsPage.js
+++ src/containers/MentorsPage.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Grid } from '../components/styled/Grid';
 
+const List = styled.ul`
+  list-style: none;
+`
 
+const ListItem = styled.li`
+
+`
 
 const MentorsPage = () => {
+  const [mentors, setMentors] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/mentors')
+    .then(resp => resp.json())
+    .then(data => setMentors(data))
+    .catch((error) => console.error('Error:', error))
+  },[])
+
   return(
     <Grid>
-      
+      <List>
+        {mentors.map(mentor => <ListItem>{mentor.name}</ListItem>)}
+      </List>
     </Grid>
   )
 }
