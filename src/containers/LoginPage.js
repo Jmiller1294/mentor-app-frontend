065bf3364d38c411e0978d26320340ead2cd2824
Justin Miller <jayman1294@gmail.com>
2021-07-28 23:59:01 -0400

    added styling for login form

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -3,17 +3,33 @@ import styled from 'styled-components';
 import { Grid, Row, Col } from '../components/styled/Grid';
 
 const LoginForm = styled.form`
+  background-color: #f8f7f2;
   display: flex;
   justify-content: center;
   flex-direction: column;
   text-align: center;
-  min-height: 500px;
+  min-height: 400px;
   align-items: center;
+  margin-top: 35px;
+  width: 25%;
+  border-radius: 25px;
 `
 
 const LoginInput = styled.input`
-
+  height: 30px;
+  width: 175px;
+  border-radius: 10px;
+  margin: 5px;
 `
+
+const LoginBtn = styled.button`
+  color: #005776;
+  width: 65px;
+  height: 25px;
+  background-color: #808080;
+  border-radius: 10px;
+`
+
 
 const LoginPage = () => {
   return(
@@ -21,8 +37,9 @@ const LoginPage = () => {
       <Row>
         <Col size={1}>
           <LoginForm>
-            <LoginInput></LoginInput>
-            <LoginInput></LoginInput>
+            <LoginInput placeholder="Email"></LoginInput>
+            <LoginInput placeholder="Password"></LoginInput>
+            <LoginBtn>Login</LoginBtn>
           </LoginForm>
         </Col>
       </Row>
