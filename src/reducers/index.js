4ebba0a9ae35a77b4d80a9a27edef14e4dd9c2dc
Justin Miller <jayman1294@gmail.com>
2021-07-29 22:36:05 -0400

    created rootReducer for user login status

---
+++ src/reducers/index.js
@@ -0,0 +1,37 @@
+const INITIAL_STATE = {
+  currentUser: null,
+  loggedIn: false
+}
+
+
+export const rootReducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    case "CREATE_USER":
+      return {
+        ...state,
+        currentUser: action.payload.user,
+        loggedIn: true
+      }
+    case "SET_CURRENT_USER":
+      return {
+        ...state,
+        currentUser: action.payload.user,
+        loggedIn: action.payload.logged_in
+      }
+    case "LOGIN":
+      return {
+        ...state,
+        currentUser: action.payload.user,
+        loggedIn: action.payload.logged_in
+      }
+    case "LOGOUT":
+      return {
+        ...state,
+        currentUser: null,
+        loggedIn: false
+      }
+
+    default: 
+      return state;
+  }
+}
