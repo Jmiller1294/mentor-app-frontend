6aaacf3a55e6397d18bdd4bed7a495f235c89200
Justin Miller <jayman1294@gmail.com>
2021-07-29 23:10:45 -0400

    created startLogin and finishLoginactions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -8,8 +8,30 @@ export const checkLoggedInStatues = () => {
   }
 }
 
-const setCurrentUser = (data) => ({
+export const setCurrentUser = (data) => ({
   type: "SET_CURRENT_USER",
   payload: data
 })
+
+export const startLogin = (data) => {
+  return(dispatch) => {
+    fetch("http://localhost:3001/sessions", {
+      method: "POST",
+      credentials: "include",
+      headers: { 
+        "Content-type": "application/json",
+        body: JSON.stringify(data)
+      }
+    })
+    .then(resp => resp.json())
+    .then(data => dispatch(finishLogin(data)))
+  }
+}
+
+export const finishLogin = (data) => ({
+  type: "LOGIN",
+  payload: data
+})
+
+
 
