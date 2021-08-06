1505daa37896bc538b5b0d251a9cb5a7d3593044
Justin Miller <jayman1294@gmail.com>
2021-08-05 23:32:01 -0400

    added styling to mentor component

--- src/components/mentors/mentor.js
+++ src/components/mentors/mentor.js
@@ -8,25 +8,39 @@ const ListItem = styled.li`
   border: 1px solid #f8f7f2;
   padding: 20px;
 `
-
 const AvatarImage = styled.img`
+  display: flex;
+  float: left;
   height: 100px;
   width: 5%;
   border-radius: 50px;
+  margin-right: 30px;
+`
+const Header = styled.h2`
+  height: 10px;
+  display: block;
+`
+const Bio = styled.p`
+  display: block;
+`
+const InfoContainer = styled.div`
+  height: 100px;
 `
 
 let faker = require('faker')
 
 const Mentor = ({ id, name, title, field}) => {
   return (
-    <div>
-      <ListItem key={id}>
-          <AvatarImage alt="avatar" src={faker.image.avatar()}></AvatarImage>
-          {name} <br></br>
+    <ListItem key={id}>
+      <AvatarImage alt="avatar" src={faker.image.avatar()}></AvatarImage>
+      <InfoContainer>
+        <Header>{name}</Header>
+        <Bio>
           {title} <br></br>
-          {field}
-        </ListItem>)}
-    </div>
+          Industry: {field}
+        </Bio>
+      </InfoContainer>
+    </ListItem>
   )
 }
 export default Mentor;
