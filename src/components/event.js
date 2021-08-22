97d8d068283cd7581a10d769b956b02e2a84dff2
Justin Miller <jayman1294@gmail.com>
2021-08-22 00:12:56 -0400

    added createAttendance from actions to the event component

--- src/components/event.js
+++ src/components/event.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { useDispatch,useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { createAttendance } from '../actions/userActions';
 
 const AddButton = styled.button`
   background-color: orange;
   height: 40px;
   width: 65px;
 `
-
 
 const Event = ({ event }) => {
   const user = useSelector(state => state.currentUser);
@@ -26,8 +24,7 @@ const Event = ({ event }) => {
       .then(event => console.log(event))
     }
 
-  const handleClick = (evt) => {
-    evt.preventDefault();
+  const handleClick = () => {
     createAttendance(event, user.id);
   }
 
@@ -39,7 +36,7 @@ const Event = ({ event }) => {
       {event.location}<br></br>
       {event.description}<br></br>
       {event.likes} likes<br></br>
-      {user ? <AddButton onClick={(e) => handleClick(e)}>Add Event</AddButton> : null}
+      {user ? <AddButton onClick={() => handleClick()}>Add Event</AddButton> : null}
     </div>
   )
 }
