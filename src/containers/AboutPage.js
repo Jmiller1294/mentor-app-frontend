e65e7917e2dac519696c2a7bbd842728ff9bac54
Justin Miller <jayman1294@gmail.com>
2021-09-05 00:22:24 -0400

    added mission statement and leadership

--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -13,12 +13,30 @@ const BackgroundImg = styled.img`
   width: 100%;
   height: 100%;
 `
+const MissionStatement = styled.p`
+  text-align: center;
+  font-size: 28px;
+  margin: 30px 280px;
+`
+const LeadershipCon = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 400px;
+  width: 100%;
+`
+const Leaders = styled.img`
+  display: flex;
+  flex-direction: column;
+  height: 200px;
+  width: 200px;
+  margin: 30px;
+`
 
 const AboutPage = () => {
 
   return(
     <Grid>
-      <Row height={'450px'}>
+      <Row height={'470px'}>
         <Col size={1}>
           <BackgroundImg src={backgroundPic}></BackgroundImg>
         </Col>
@@ -26,7 +44,29 @@ const AboutPage = () => {
       <Row>
         <Col size={1}>
           <Header>Our Mission</Header>
+          <MissionStatement>
+            hghjghjgjhkhghjghjghjghjghgffffffff
+            hghghjghjghjghjghjghjghjgjhgddddddd
+            ghkjghjjghgjhghjghjghjghgjhgh
+            dkjfksdfjkdsjfkdsjfkjskfjkfksd
+            sfsdfdslfsdlfksdlfklsdkfldskf
+            sdlfksdlfkldskfldskfldsklfkdsl
+            sdfdsfdsfsdfsfsdfdsfsfsdfdssdf
+          </MissionStatement>
         </Col>
+      </Row>
+      <Row>
+       <LeadershipCon>
+         <Leaders>
+
+         </Leaders>
+         <Leaders>
+
+         </Leaders>
+         <Leaders>
+           
+         </Leaders>
+       </LeadershipCon>
       </Row>
     </Grid>
   )
