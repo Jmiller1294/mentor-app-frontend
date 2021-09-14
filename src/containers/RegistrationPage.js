12f0954ae391edc923f65774d9237f1bf6f739ea
Justin Miller <jayman1294@gmail.com>
2021-09-14 01:12:08 -0400

    added controlled registration form

--- src/containers/RegistrationPage.js
+++ src/containers/RegistrationPage.js
@@ -1,10 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { startCreateUser } from '../actions/userActions';
+import styled from 'styled-components';
+import { Grid, Row, Col } from '../components/styled/Grid';
+
+const RegistrationForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  background-color: #f8f7f2;
+  height: 50%;
+  flex-basis: 35%;
+  border-radius: 20px;
+  justify-content: center;
+`
+const FormInput = styled.input` 
+  height: 10%;
+  width: 50%;
+  align-self: center;
+  margin: 10px;
+`
+const RegistrationBtn = styled.button`
+  color: #005776;
+  width: 25%;
+  height: 35px;
+  background-color: #808080;
+  border-radius: 5px;
+  font-size: 20px;
+  align-self: center;
+`
+const RegistrationContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 700px;
+`
 
 const RegistrationPage = () => {
+  const [registrationInfo, setRegistrationInfo] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('clicked', registrationInfo);
+    dispatch(startCreateUser(registrationInfo));
+  }
+
+  const handleInputChange = (event) => {
+    setRegistrationInfo({...registrationInfo, [event.target.name]: event.target.value})
+  }
+  
   return(
-    <h1>
-      RegistrationPage
-    </h1>
+    <Grid>
+      <Row>
+        <Col size={1}>
+          <RegistrationContainer>
+            <RegistrationForm>
+              <h2 style={{textAlign: 'center'}}>Register</h2>
+              <FormInput 
+                type="text" 
+                name="name" 
+                value={registrationInfo.name} 
+                placeholder="Full Name"
+                onChange={(e) => handleInputChange(e)} 
+              >
+              </FormInput>
+              <FormInput 
+                type="text" 
+                name="email" 
+                value={registrationInfo.email} 
+                placeholder="Email"
+                onChange={(e) => handleInputChange(e)} 
+              >
+              </FormInput>
+              <FormInput 
+                type="password" 
+                name="password" 
+                value={registrationInfo.password} 
+                placeholder="Password"
+                onChange={(e) => handleInputChange(e)} 
+              >
+              </FormInput>
+              <RegistrationBtn onClick={(e) => handleSubmit(e)}>Register</RegistrationBtn>
+            </RegistrationForm>
+          </RegistrationContainer>
+        </Col>
+      </Row>
+    </Grid>
   )
 }
 export default RegistrationPage;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -10,7 +10,6 @@ import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-//creates Redux Store
 const store = createStore(rootReducer, 
   composeEnhancers(applyMiddleware(thunk))
 );
